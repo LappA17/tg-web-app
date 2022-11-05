f4d28afa09220eaf00ae52d701b2c22234454b74
Ruslan <ruslan.postoyuk@gmail.com>
2022-11-05 13:22:43 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, {useEffect} from 'react';
-const tg = window.Telegram.WebApp;
+
 const App = () => {
     useEffect(() => {
         // ready сообщает что приложение полностью приинициализировалось и его можно отрисовывать
         tg.ready()
     }, [])
-    const onClose = () => {
-        tg.close()
-    }
+
     return (
         <button onClick={onClose}>
             Close
