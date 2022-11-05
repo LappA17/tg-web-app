3eb45f594a7c7dbec4a332aff02cdbf33c60c4ad
Ruslan <ruslan.postoyuk@gmail.com>
2022-11-05 14:48:47 +0100

    changes5

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, {useEffect} from 'react';
 import {useTelegram} from "./components/hooks/useTelegram";
 import Header from "./components/Header/Header";
+import {Route, Routes} from 'react-router-dom'
+import ProductList from "./components/ProductList/ProductList";
+import Form from "./components/Form/Form";
 
 const App = () => {
     const {tg, onToggleButton} = useTelegram()
@@ -12,9 +15,16 @@ const App = () => {
     return (
         <div className='App'>
             <Header/>
-            <button onClick={onToggleButton}>Toggle</button>
+            <Routes>
+                <Route index element={<ProductList />}/>
+                <Route path={'form'} element={<Form />}/>
+            </Routes>
         </div>
     );
 };
 
 export default App;
+
+/*
+    Мы создали файл netlify.toml потому что Реакт у нас single page application, а наш сервер notlify который раздаёт нам статику пытается найти маршрут /form а такого маршрута не существует, потому что, потому что все маршруты у нас искуственные и сделаны с Реакт-роутер-дом
+ */
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,78 @@
+import React, {useCallback, useEffect, useState} from 'react';
+import './Form.css';
+import {useTelegram} from "../hooks/useTelegram";
+
+const Form = () => {
+    const [country, setCountry] = useState('');
+    const [street, setStreet] = useState('');
+    const [subject, setSubject] = useState('physical');
+    const {tg} = useTelegram();
+
+    //мы используем useCallback что бы сохранить ссылку на фции + что бы при каждой перерисовку не создавалась новая фция
+    const onSendData = useCallback(() => {
+        const data = {
+            country,
+            street,
+            subject
+        }
+        tg.sendData(JSON.stringify(data));
+    }, [country, street, subject])
+
+    useEffect(() => {
+        tg.onEvent('mainButtonClicked', onSendData)
+        return () => {
+            tg.offEvent('mainButtonClicked', onSendData)
+        }
+    }, [onSendData])
+
+    useEffect(() => {
+        tg.MainButton.setParams({
+            text: 'Send data'
+        })
+    }, [])
+
+    useEffect(() => {
+        if(!street || !country) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
+        }
+    }, [country, street])
+
+    const onChangeCountry = (e) => {
+        setCountry(e.target.value)
+    }
+
+    const onChangeStreet = (e) => {
+        setStreet(e.target.value)
+    }
+
+    const onChangeSubject = (e) => {
+        setSubject(e.target.value)
+    }
+    return (
+        <div className={"form"}>
+            <h3>Enter your data</h3>
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'Страна'}
+                value={country}
+                onChange={onChangeCountry}
+            />
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'Street'}
+                value={street}
+                onChange={onChangeStreet}
+            />
+            <select value={subject} onChange={onChangeSubject} className={'select'}>
+                <option value={'physical'}>Physical person</option>
+                <option value={'legal'}>Legal</option>
+            </select>
+        </div>
+    );
+};
+
+export default Form;
